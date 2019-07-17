cc2bf8e4247189a4d14c9e04ceebd8c0b991dbba
Kurl <kurl.palaganas@aiah.ai>
2019-07-17 16:16:00 +0800

    added generation on invalid name

--- src/data_types/names.py
+++ src/data_types/names.py
@@ -1,22 +1,45 @@
+from random import choice
+
 from src.data_types.base import Data
 
 
-class Name(Data):
+class Name:
+    instance = str
+
+    @property
+    def invalid_name(self):
+        invalid_names = [
+            '',
+            '    ',
+            99,
+            69,
+            68
+        ]
+
+        return choice(invalid_names)
+
+
+class FullName(Data, Name):
     def generate_valid_data(self):
         return self.faker.name()
 
+    def generate_invalid_data(self):
+        return self.invalid_name
 
-class FirstName(Data):
+
+class FirstName(Data, Name):
     # TODO: Gender based name
     def generate_valid_data(self):
         return self.faker.first_name()
 
+    def generate_invalid_data(self):
+        return self.invalid_name
 
-class LastName(Data):
+
+class LastName(Data, Name):
     # TODO: Gender based name
     def generate_valid_data(self):
         return self.faker.last_name()
 
-
-if __name__ == '__main__':
-    print(Name().generate_valid_data())
+    def generate_invalid_data(self):
+        return self.invalid_name
