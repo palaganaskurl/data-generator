52d479c8c5fad4fcac327330fc663537bc041a92
Kurl <kurl.palaganas@aiah.ai>
2019-07-17 16:15:06 +0800

    added generate_invalid_data abstract method added generating invalid data instance method

--- src/data_types/base.py
+++ src/data_types/base.py
@@ -1,4 +1,6 @@
 from abc import ABCMeta, abstractmethod
+from random import choice
+
 from faker import Faker
 
 
@@ -8,3 +10,20 @@ class Data(metaclass=ABCMeta):
     @abstractmethod
     def generate_valid_data(self):
         raise NotImplementedError
+
+    @abstractmethod
+    def generate_invalid_data(self):
+        raise NotImplementedError
+
+    def generate_invalid_data_instance(self, valid_instance):
+        instances = {
+            bool: self.faker.pybool(),
+            float: self.faker.pydecimal(),
+            int: self.faker.pyint(),
+            list: self.faker.pylist(6, True, 'str', 'float', 'int', 'decimal', 'uri', 'email')
+        }
+
+        if valid_instance in instances:
+            del instances[valid_instance]
+
+        return choice(list(instances.values()))
