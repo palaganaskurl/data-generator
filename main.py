e7f6f2e39b5cb52d9f89c51c27f03ba0d1765f2a
Kurl <kurl.palaganas@aiah.ai>
2019-07-17 16:14:18 +0800

    main test

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from src.data_generator import DataGenerator
-from src.data_types.names import Name, FirstName, LastName
+from src.data_generator import ValidDataGenerator, InvalidDataGenerator
+from src.data_types.names import FullName, FirstName, LastName
 
 
 if __name__ == '__main__':
-    d = DataGenerator({
-        'name': Name(),
+    # d = ValidDataGenerator({
+    #     'name': Name(),
+    #     'full_name': {
+    #         'first_name': FirstName(),
+    #         'last_name': LastName(),
+    #         'others': {
+    #             'suffix': Name()
+    #         }
+    #     },
+    #     'full_name_xd': {
+    #         'first_name': FirstName(),
+    #         'last_name': LastName(),
+    #         'others': {
+    #             'suffix': Name()
+    #         }
+    #     }
+    # })
+    # import pprint
+    # pprint.pprint(d.generate_valid_data_for_create())
+    d = InvalidDataGenerator({
+        'name': FullName(),
         'full_name': {
             'first_name': FirstName(),
-            'last_name': LastName()
+            'last_name': LastName(),
+            'others': {
+                'suffix': FullName()
+            }
+        },
+        'full_name_xd': {
+            'first_name': FirstName(),
+            'last_name': LastName(),
+            'others': {
+                'suffix': FullName()
+            }
         }
     })
-    print(d.generate_valid_data('a', {}))
+    import pprint
+    x = d.generate_invalid_data_for_create()
+    pprint.pprint(x)
+    d.print_to_json(x)
